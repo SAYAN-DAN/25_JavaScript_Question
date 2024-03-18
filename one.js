fff9f8b78760b10ed77f8bc72b9f5d036c4944b6
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-03-18 10:09:53 +0530

    one

---
+++ one.js
@@ -0,0 +1,9 @@
+const str = "Hello world"
+// console.log(str.split(" ").reverse())
+
+const str2 = "HelloWorld"
+// console.log(str2.split("").reverse());
+// console.log(str2.split("").reverse().join());
+console.log(str2.split("r"));
+console.log(str2.split("w"));
+console.log(str2.split("o"));
