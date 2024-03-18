bc2b18b551cdfadda7c32a0cacf733e97738cfe9
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-03-18 10:10:33 +0530

    four

---
+++ four.js
@@ -0,0 +1,15 @@
+
+// console.log(Number.isInteger(1.2));
+// console.log(Number.isInteger(2));
+
+//  % => The mod operator, denoted by %, is used to find the remainder after performing integer division.
+//   12/1 => Here we don't get any remainder.
+    //  12.5/1 => Here we get .5 remainder.
+
+let num = 16
+if (num % 1 === 0) {
+    console.log("This is a Integer");
+}
+else{
+    console.log("This is not a Integer");
+}
