9c5d80a128090e7862a2269e26ba7aeb0e189a82
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-03-18 10:10:43 +0530

    five

---
+++ five.js
@@ -0,0 +1,15 @@
+function duplicate(arr) {
+  return arr.concat(arr);
+}
+const number = [1, 2, 3, 4, 5];
+const newnum = duplicate(number);
+// console.log(newnum);
+
+
+
+
+
+function duplicate(arr) {
+  return arr.concat(arr);
+}
+console.log(duplicate([1,2,3,4,5]));
